6c74564d8c947e3d8ebab0e155b01552c7e08b9c
Saswat1999 <47583914+Saswat1999@users.noreply.github.com>
2019-07-28 17:54:28 +0530

    Add files via upload

---
+++ Water Level/waterlevel3/waterlevel3.ino
@@ -0,0 +1,55 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+ 
+const char* ssid = "prateek";
+const char* password = "100sorry"; 
+  const int trigPin = 2;
+const int echoPin = 0;
+
+float duration, distance;
+void setup(void)
+{ 
+
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+ 
+    delay(1000);
+    Serial.println("Connecting..");
+ 
+  }
+
+}
+void loop() {
+   digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(100); 
+    if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+ 
+    HTTPClient http;  //Declare an object of class HTTPClient
+ 
+    http.begin("http://3.17.13.174:8080/WaterLevel/ultra.jsp?id=3&distance="+String(distance));
+    Serial.print(distance);//Specify request destination
+    int httpCode = http.GET();                                                                  //Send the request
+ 
+    if (httpCode > 0) { //Check the returning code
+    }
+    http.end();   //Close connection
+  delay(800);
+    }
+}
+
+
+ 
